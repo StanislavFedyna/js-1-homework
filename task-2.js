93d0991a83633bef08900e95275ab40a648edd27
StanislavFedyna <stanislavfedyna@gmail.com>
2019-01-21 13:13:01 +0200

    finished task 2

--- task-2.js
+++ task-2.js
@@ -1,7 +1,32 @@
-
-
-function generateTable(columns, rows){
-  var table = document.createElement("table");
-  var tableBody = document.createElement("tbody");
+const renderTable = (columns, rows) =>{
+  let table = '';
+  
+  //Crating columns and rows
+  for(let r = 0; r < rows; r++){
+    table += '<tr>'
+    for(let c = 0; c < columns; c++){
+      table += '<td>' + '</td>'
+    }
+    '</tr>'
+  }
+    //Render to the page
+    document.write('<table>' + table + '</table>')
+    
+    // Numbering
+    let tdElements = document.querySelectorAll('td');
+      for(let i = 0; i < tdElements.length; i++){
+     tdElements[i].textContent = i + 1;
+   }
+    
+    //Delegation for table 
+    let tableEvent = document.querySelector('table');
+    tableEvent.addEventListener('click', showNumber);
+    
+    function showNumber(event){
+      let target = event.target;
+      alert(target.textContent)
+    }
 }
+
+renderTable(3,5)
   
